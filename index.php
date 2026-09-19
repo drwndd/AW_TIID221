c0b07f5b98550833c253e5ce7d451507be1fa34d
drwndd <125053509@upq.edu.mx>
2026-09-18 19:37:42 -0600

    Se usaron archivos php y css para agregar estilos a una pagina html

--- index.php
+++ index.php
@@ -8,19 +8,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        h1 {
-            color: #5e0075;
-        }
-    </style>
+
+    <link rel="stylesheet" href="styles.css">
+ 
 </head>
 <body>
+
+<div>   
     <h1>Oliwili Pili</h1>
     <h2>Oliwili Pili</h2>
     <h3>Oliwili Pili</h3>
     <h4>Oliwili Pili</h4>
     <h5>Oliwili Pili</h5>
     <h6>Oliwili Pili</h6>
+</div>
+    <p>Estoy aprendiendo a desarrollar web con PHP, CSS y HTML  </p>
 
+    <div>
+        <h2>Mi seccion</h2>
+
+        <p>Este es el contenido de mi seccion </p>
+
+    
+    </div>
+    <div class="section">
+        <h2>Titulo del parrafo</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, voluptatibus modi odit doloremque dicta odio amet reprehenderit iusto molestias cum nam obcaecati nisi commodi voluptatum ullam sed voluptates inventore sint?</p>
+
+</div>
+    <div class="seccion">
+        <h2>Titulo del parrafo</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, voluptatibus modi odit doloremque dicta odio amet reprehenderit iusto molestias cum nam obcaecati nisi commodi voluptatum ullam sed voluptates inventore sint?</p>
+</div>
 </body>
 </html>
